3994ccac1a947626c63673d7e50ae550ffdf215e
shrea1456 <shreashidutta1406@gmail.com>
2026-02-23 23:28:28 +0530

    Update class.py

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-class Ankur:
+class Shreashi:
     def __init__(self, data):
         self.data = data
         self.left = None
@@ -8,11 +8,12 @@ class Ankur:
 root = None
 a = [1, 10, 11, 8, 6, 12, 14, 7]
 print(root)
-root = Ankur(a[0])
+root = Shreashi(a[0])
 print(root)
 print(root.data)
-b = Ankur(a[1])
+b = Shreashi(a[1])
 print(b)
 print(b.data)
         
+
 
